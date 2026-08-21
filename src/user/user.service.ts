0b2b12beb1e4ad46769322a47f672b246ace0b64
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-08-21 21:08:24 +0530

    feat(controller): add use end points

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  // mock user list
+  private user = [
+    {
+      id: 1,
+      name: 'Lucky',
+      type: 'normal',
+    },
+    {
+      id: 2,
+      name: 'Nikhil',
+      type: 'celebrity',
+    },
+  ];
+
+  // return all users
+  getAllUser() {
+    return this.user;
+  }
+
+  // return specific user
+  getUser(id: number) {
+    return this.user.find((user) => user.id === id);
+  }
+}
