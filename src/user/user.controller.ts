90ff0cf781d0006a0f970662b47e4b64f35f3b96
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-08-21 21:09:09 +0530

    feat(service): add service for user controller end points

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    // dependency injection
+    constructor (private readonly userService : UserService) {};
+
+    @Get()
+    getAllUser(){
+        // const userService = new UserService();
+        return this.userService.getAllUser();
+    }
+
+    @Get(":id")
+    getUser(@Param('id') id: String){
+        // const userService = new UserService();
+        return this.userService.getUser(+id);
+    }
+
+
+    /*
+    GET (user/) - retrun all users
+    GET (user/:id) - return specific user
+    POST (user) - add a user
+    */
+
+    // in controller we chekc whether the paylod is correctly passed if not then it will give respective error else it will pass to service for logic processsing
+}
