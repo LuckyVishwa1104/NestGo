6c825e56ccdcf22fbd58dfa4d22f456357e8a8f5
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-02 23:22:44 +0530

    feat(service): add logic for CRUD todo methods

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { TodoDto } from './dto/todo.dto';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    private todos = [
+        {
+            id: 1,
+            title: "Todo 1",
+            body: "Body for Todo1"
+        },
+        {
+            id: 2,
+            title: "Todo 2",
+            body: "Body for Todo2"
+        }
+    ]
+
+    // get all todo
+    getAllTodo(){
+        return this.todos;
+    }
+
+    // get specific todo
+    getTodo(id: number){
+        const todo = this.todos.find((todo) => todo.id === id);  
+        if(!todo){
+            throw new Error("Todo not found.");
+        }
+        return todo;
+    }
+
+    // add new todo
+    addTodo(todo: TodoDto){
+        const id = Date.now();
+        this.todos.push({
+            id,
+            ...todo,
+        });
+
+        return this.getTodo(id);
+    }
+}
