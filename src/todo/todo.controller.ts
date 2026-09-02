4c0826a5e163d220aabc384e336c9946ae621d9a
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-02 23:34:00 +0530

    feat(controller): add contorller for CRUD for todo operation

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { TodoDto } from './dto/todo.dto';
 
 @Controller('todo')
 export class TodoController {
+
+  // dependecy injection of todo service
   constructor(private readonly todoService: TodoService) {}
+
+  // get all todo
+  @Get()
+  getAllTodo(){
+    return this.todoService.getAllTodo();
+  }
+
+  // get specific todo
+  @Get(":id")
+  getTodo(@Param('id') id: string){
+
+    try{
+      return this.todoService.getTodo(+id);
+    }
+    catch(error){
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  // add todo
+  @Post()
+  addTodo(@Body() todo: TodoDto){
+    return this.todoService.addTodo(todo);  
+  }
 }
