8f20e784e2f0ff09957b279872365cea64be43e7
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-02 22:49:40 +0530

    chore: add initial for book and todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [UserModule, BookModule],
+  imports: [UserModule, BookModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
