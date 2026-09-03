9bfe97fd167afadfaa7fb790ed78ad03a842386a
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-03 23:52:42 +0530

    feat(controller): add controller endpoints for book module

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { BookService } from './book.service';
+import { BookDto } from './dto/book.dto';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  // get all book
+  @Get()
+  getAllBook() {
+    return this.bookService.getAllBook();
+  }
+
+  // get book
+  @Get(':id')
+  getBook(@Param('id') id: string) {
+    try {
+      return this.bookService.getBook(+id);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  // add new book
+  @Post()
+  addBook(@Body() book: BookDto) {
+    return this.bookService.addBook(book);
+  }
+}
