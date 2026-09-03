0b92ace5a4eef5a87de71ce67d231c8c791d585a
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-03 23:52:03 +0530

    feat(service): add logic for book API

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { BookDto } from './dto/book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  // mock test data
+  private books = [
+    {
+      id: 1,
+      title: "Can't Hurt Me",
+      author: 'David Gogging',
+    },
+    {
+      id: 1,
+      title: 'Metamorphosis',
+      author: 'Franz Kafka',
+    },
+  ];
+
+  // get all books
+  getAllBook() {
+    return this.books;
+  }
+
+  // get specific book
+  getBook(id: number) {
+    const book = this.books.find((book) => book.id === id);
+    if (!book) {
+      throw new Error('Book not found!');
+    }
+    return book;
+  }
+
+  // add new book
+  addBook(book: BookDto){
+    const id  = Date.now();
+    this.books.push({
+        id,
+        ...book,
+    });
+
+    return this.getBook(id);
+  }
+}
