c067345fd229ceaa8afdd9486c6b1aa2aa9a0be4
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-06 14:24:22 +0530

    feat(guard): add guard validation for book module

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,16 +6,19 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookDto } from './dto/book.dto';
 import { truncate } from 'fs';
+import { FirewallGuard } from 'src/firewall/firewall.guard';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  // for get - pipe --> controller --> service
   // get all book
   @Get()
   getAllBook() {
@@ -24,7 +27,6 @@ export class BookController {
 
   // get book
   @Get(':id')
-
   // Pipe - used to transform and validate param data
   // for validator we need to dev dependency - class-validator and class-transformer
   getBook(@Param('id', ParseIntPipe) id: number) {
@@ -35,8 +37,10 @@ export class BookController {
     }
   }
 
+  // for post - guard --> pipe --> controller --> service
   // add new book
   @Post()
+  @UseGuards(FirewallGuard)
   addBook(@Body(new ValidationPipe({transform : true})) book: BookDto) {
     return this.bookService.addBook(book);
   }
