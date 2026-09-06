6607d2604e2500672668d5077a346ceb7487e722
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-09-06 19:05:02 +0530

    feat(validator): add parameter validator for todo dto

--- src/todo/dto/todo.dto.ts
+++ src/todo/dto/todo.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, MinLength } from "class-validator";
+
 export class TodoDto {
+    @IsString()
+    @MinLength(3)
     title: string;
+
+    @IsString()
     body: string;
+
+    @IsString()
+    status: string;
 }
